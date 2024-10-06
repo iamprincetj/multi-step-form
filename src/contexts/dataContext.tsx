93d6c7fea6de0dc8d91f0692e4b0bf4b1b1de86a
iamprincetj <tochukwunwanze5@gmail.com>
2024-10-06 16:40:20 +0100

    updtaed

--- src/contexts/dataContext.tsx
+++ src/contexts/dataContext.tsx
@@ -2,10 +2,14 @@ import React, { useReducer } from "react";
 import { AddOnContext, addOnReducer, initialState } from "./constants";
 import { Action, AddOnProviderProp, ReducerData, ReturnedData } from "../types";
 
+const initializer = (initialValue: ReturnedData) => {
+    return initialValue;
+};
+
 
 
 export const AddOnProvider: React.FC<AddOnProviderProp> = ({ children }) => {
-    const [value, dispatch]: ReducerData = useReducer<(state: ReturnedData, action: Action) => ReturnedData | never[], ReturnedData>(addOnReducer, initialState);
+    const [value, dispatch]: ReducerData = useReducer<(state: ReturnedData, action: Action) => ReturnedData, ReturnedData>(addOnReducer, initialState, initializer);
 
     return (
         <AddOnContext.Provider value={{ state: value, dispatch }}>
