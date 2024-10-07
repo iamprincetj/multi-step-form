67be288cbdc8bb37a0be25dafd16705e2e5ebca6
iamprincetj <tochukwunwanze5@gmail.com>
2024-10-07 14:56:01 +0100

    updated

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,4 +1,4 @@
-import { Link, useMatch } from 'react-router-dom';
+import { useMatch } from 'react-router-dom';
 import '../assets/css/SideBar.css';
 
 const SideBar = () => {
@@ -13,34 +13,34 @@ const SideBar = () => {
   return (
     <aside className='nav-container'>
       <ul className='nav-list'>
-        <span className='nav-item'>
+        <li className='nav-item'>
           <span id={step1 ? 'active': ''}>1</span>
           <div className='nav-text'>
             <span>STEP 1</span>
             <p>YOUR INFO</p>
           </div>
-        </span>
-        <span className='nav-item'>
+        </li>
+        <li className='nav-item'>
           <span id={step2 ? 'active': ''}>2</span>
           <div className='nav-text'>
             <span>STEP 2</span>
             <p>SELECT PLAN</p>
           </div>
-        </span>
-        <span className='nav-item'>
+        </li>
+        <li className='nav-item'>
           <span id={step3 ? 'active': ''}>3</span>
           <div className='nav-text'>
             <span>STEP 3</span>
             <p>ADD-ONS</p>
           </div>
-        </span>
-        <span className='nav-item'>
+        </li>
+        <li className='nav-item'>
           <span id={step4 || thankyou ? 'active': ''}>4</span>
           <div className='nav-text'>
             <span>STEP 4</span>
             <p>SUMMARY</p>
           </div>
-        </span>
+        </li>
       </ul>
     </aside>
   );
