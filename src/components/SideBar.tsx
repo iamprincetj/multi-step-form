41494e98bcbb1a6bb750052b62982de8dff209e7
iamprincetj <tochukwunwanze5@gmail.com>
2024-10-07 14:44:46 +0100

    updated

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -13,34 +13,34 @@ const SideBar = () => {
   return (
     <aside className='nav-container'>
       <ul className='nav-list'>
-        <Link to='/' className='nav-item'>
+        <span className='nav-item'>
           <span id={step1 ? 'active': ''}>1</span>
           <div className='nav-text'>
             <span>STEP 1</span>
             <p>YOUR INFO</p>
           </div>
-        </Link>
-        <Link to='/step2' className='nav-item'>
+        </span>
+        <span className='nav-item'>
           <span id={step2 ? 'active': ''}>2</span>
           <div className='nav-text'>
             <span>STEP 2</span>
             <p>SELECT PLAN</p>
           </div>
-        </Link>
-        <Link to='/step3' className='nav-item'>
+        </span>
+        <span className='nav-item'>
           <span id={step3 ? 'active': ''}>3</span>
           <div className='nav-text'>
             <span>STEP 3</span>
             <p>ADD-ONS</p>
           </div>
-        </Link>
-        <Link to='/step4' className='nav-item'>
+        </span>
+        <span className='nav-item'>
           <span id={step4 || thankyou ? 'active': ''}>4</span>
           <div className='nav-text'>
             <span>STEP 4</span>
             <p>SUMMARY</p>
           </div>
-        </Link>
+        </span>
       </ul>
     </aside>
   );
